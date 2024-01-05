36132295b0d5ce5e1b5889974004b7ad6a60f756
Murilo Fujita <murilofujita@gmail.com>
2024-01-04 23:18:52 -0300

    1ª versão Python BuscaRaizes

---
+++ Matematica/Python/BuscaRaizes.py
@@ -0,0 +1,35 @@
+import sys
+
+def localizaRaizEntre2Inteiros(Y):
+    i=0
+    for item in Y.keys():
+        print (item[i], item[i+1])
+
+def armazenaSinais(esq, dir):
+    
+    parOrdenado = {}
+
+    for x in range(esq,dir+1):
+        parOrdenado[x] = 1.4*x**3 - 3.2*x**2 - 14.8*x + 23.6
+    
+    for y in parOrdenado.keys():
+        print(y)
+    
+    print()
+    for y in parOrdenado.values():
+        print(y)
+    print()
+
+    localizaRaizEntre2Inteiros(parOrdenado)
+
+def main():
+    esquerdo = int(sys.argv[1])
+    direito = int(sys.argv[2])
+    
+    armazenaSinais(esquerdo,direito)
+
+if __name__ == "__main__":
+    main()
+
+
+        
