c92f4897031574acd851ae9c94d02ece10f0870b
Murilo Fujita <murilofujita@gmail.com>
2024-01-15 13:32:52 -0300

    Python processa equação por linha de comando

--- Matematica/Python/BuscaRaizes.py
+++ Matematica/Python/BuscaRaizes.py
@@ -1,31 +1,30 @@
 import sys
 
-def localizaRaizes(esq, dir, eq):
+def main():
+    try:
+        equacao = sys.argv[1]
+        esquerdo = int(sys.argv[2])
+        direito = int(sys.argv[3])
+        
+        localizaRaizes(equacao,esquerdo,direito)
+    except IndexError:
+        print("  Use BuscaRaizes.py [equacao] [limite_esquerdo] [limite_direito] ")
+        print("  Exemplo:")
+        print("     BuscaRaizes.py 1.4*x**3-3.2*x**2-14.8*x+23.6 -10 10")
+
+def localizaRaizes(eq, esq, dir):
     parOrdenado = []
 
     for x in range(esq,dir+1):
-        parOrdenado.append(1.4*x**3-3.2*x**2-14.8*x+23.6)
+        parOrdenado.append(eval(eq))
 
     print('Há raizes entre os valores:')
     i=0
     while i < len(parOrdenado)-1:
-        #print(parOrdenado[i])
         if float(parOrdenado[i]) > 0 and float(parOrdenado[i+1]) < 0 or parOrdenado[i] < 0 and parOrdenado[i+1] > 0:
             print(f"{esq};{esq+1}")
         i+=1
         esq+=1
-
-def main():
-    try:
-        esquerdo = int(sys.argv[1])
-        direito = int(sys.argv[2])
-        equacao = sys.argv[3]
-        
-        localizaRaizes(esquerdo,direito,equacao)
-    except IndexError:
-        print("Use BuscaRaizes.py [limite_esquedo] [limite_direito] [equacao]")
-        print("Exemplo:")
-        print("BuscaRaizes.py -10 10 1.4*x**3-3.2*x**2-14.8*x+23.6")
 
 if __name__ == "__main__":
     main()
